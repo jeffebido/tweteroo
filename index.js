f8e2356e0ece1b33f4003c89553747fcfaf43dcb
Jefferson Oliveira <jeffebido@hotmail.com>
2022-06-19 08:35:17 -0300

    Requisitos Finalizados

--- index.js
+++ index.js
@@ -4,38 +4,40 @@ import cors from 'cors';
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+
 /* Variáveis Globais */
-
 let users = [];
-
-
+let tweets = [];
 
 
 /* Rotas */
 app.post('/sign-up', (req, res) => {
 
     users.push(req.body);
-    
-
     res.send("OK");
 });
 
 app.post('/tweets', (req, res) => {
 
-
+    tweets.push(req.body);
+    res.send("OK");
 });
 
 app.get('/tweets', (req, res) => {
-    
+
+    let tweetsWithAvatar = tweets.map(function(tweet){
+
+        let img = users.filter( usr => usr.username === tweet.username );
+
+        return {
+            username: tweet.username, 
+            avatar: img[0].avatar,
+            tweet: tweet.tweet
+        };
+     });
+
+    res.send(tweetsWithAvatar);
 });
-
-
-app.get('/', (req, res) => { //Rota para testes EXCLUIR DEPOIS
-    res.send(users);
-});
-
-
-
-
 
 app.listen(5000, '127.0.0.1');
