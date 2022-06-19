87724049710ff64efa878b380a30a3bcdf47e326
Jefferson Oliveira <jeffebido@hotmail.com>
2022-06-19 11:41:45 -0300

    Paginação

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import {checkUrl, checkEmpty} from './validate.js';
 
 const app = express();
+
 app.use(express.json());
 app.use(cors());
 
@@ -13,27 +14,31 @@ let users = [];
 let tweets = [];
 
 
-/* Rotas */
+/* Rota de Cadastro*/
 app.post('/sign-up', (req, res) => {
 
-    if( checkEmpty(req.body.username) &&  checkUrl(req.body.avatar) ){
+    if( checkEmpty(req.body.username) &&  checkUrl(req.body.avatar) ){ //Valida se campos estão vazios e URL do avatar
 
         users.push(req.body);
+
         res.status(201).send("OK");
+        
     }else{
 
         res.status(400).send("Todos os campos são obrigatórios!");
     }    
 });
 
+/* Rota que salva novo Tweet*/
 app.post('/tweets', (req, res) => {
 
-    if( checkEmpty(req.body.tweet) && checkEmpty( req.header('User') ) ){
+    if( checkEmpty(req.body.tweet) && checkEmpty( req.header('User') ) ){ //Valida se campos estão vazios
 
         tweets.push({
             tweet: req.body.tweet, 
             username: req.header('User')
         });
+
         res.status(201).send("OK");
     }else{
 
@@ -41,20 +46,33 @@ app.post('/tweets', (req, res) => {
     } 
 });
 
+/* Rota que lista  Tweets*/
 app.get('/tweets', (req, res) => {    
 
-    let tweetsWithAvatar = tweets.map(function(tweet){
-        
-        let img = users.filter( usr => usr.username == tweet.username );
 
-        return {
-            username: tweet.username, 
-            avatar: img[0].avatar,
-            tweet: tweet.tweet
-        };
-     });
+    let page = req.query.page;
 
-    res.send(tweetsWithAvatar.reverse().slice(0, 10));
+    if(page && parseInt(page) > 0 ){
+
+    
+
+
+        let tweetsWithAvatar = tweets.map(function(tweet){//Relaciona avatar do usuário ao tweet
+            
+            let filteredUser = users.filter( usr => usr.username == tweet.username );//Fiiltra altor do tweet
+
+            return {
+                username: tweet.username, 
+                avatar: filteredUser[0].avatar,
+                tweet: tweet.tweet
+            };
+        });
+
+        res.send(tweetsWithAvatar.reverse().slice( (page*10)-10 , (page*10) ));//Paginação
+    }else{
+        res.status(400).send("Informe uma página válida!");
+    }
+
 });
 
 app.listen(5000, '127.0.0.1');
